44653527842c8a3ba0ce69312313fd9e74d32574
manikanta1024-git <manikantabarakati9515@gmail.com>
2026-02-04 17:24:54 +0530

    Change BUILD_ID to BUILD_NUMBER in response message

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.end('Hello from Docker + Jenkins! Build ID: ' + process.env.BUILD_ID);
+  res.end('Hello from Docker + Jenkins! Build ID: ' + process.env.BUILD_NUMBER);
 });
 server.listen(3000, () => console.log('Server running'));
