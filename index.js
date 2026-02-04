6ca76261069a3081989f5d24437802ee08694c6f
manikanta1024 <manikantabarakati9515@gmail.com>
2026-02-04 12:54:38 +0530

    My first commit message

---
+++ index.js
@@ -0,0 +1,5 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.end('Hello from Docker + Jenkins! Build ID: ' + process.env.BUILD_ID);
+});
+server.listen(3000, () => console.log('Server running'));
